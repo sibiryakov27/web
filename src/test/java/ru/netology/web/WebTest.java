91b1c27c5de0761f05e5e2fa0b007763b0f799d0
Ivan Sibiryakov <ivan.sibiryakov27@gmail.com>
2023-12-05 01:12:20 +0300

    complete tasks 1 and 2

---
+++ src/test/java/ru/netology/web/WebTest.java
@@ -0,0 +1,73 @@
+package ru.netology.web;
+
+import com.codeborne.selenide.Condition;
+import com.codeborne.selenide.SelenideElement;
+import org.junit.jupiter.api.Test;
+
+import static com.codeborne.selenide.Selenide.*;
+
+public class WebTest {
+
+    @Test
+    public void shouldSubmitTheForm() throws InterruptedException {
+        open("http://localhost:9999");
+        SelenideElement form = $(".form");
+        form.$("[data-test-id=name] input").setValue("Иванов Иван");
+        form.$("[data-test-id=phone] input").setValue("+71234567890");
+        form.$("[data-test-id=agreement]").click();
+        form.$(".button").click();
+        $("[data-test-id=order-success")
+                .shouldHave(Condition.exactText("Ваша заявка успешно отправлена! Наш менеджер свяжется с вами в ближайшее время."));
+    }
+
+    @Test
+    public void shouldValidateEmptyNameField() {
+        open("http://localhost:9999");
+        SelenideElement form = $(".form");
+        form.$(".button").click();
+        form.$(".input_invalid").$(".input__sub")
+                .shouldHave(Condition.exactText("Поле обязательно для заполнения"));
+    }
+
+    @Test
+    public void shouldValidateIncorrectName() {
+        open("http://localhost:9999");
+        SelenideElement form = $(".form");
+        form.$("[data-test-id=name] input").setValue("Ivanov Ivan");
+        form.$(".button").click();
+        form.$(".input_invalid").$(".input__sub")
+                .shouldHave(Condition.exactText("Имя и Фамилия указаные неверно. Допустимы только русские буквы, пробелы и дефисы."));
+    }
+
+    @Test
+    public void shouldValidateEmptyTelField() {
+        open("http://localhost:9999");
+        SelenideElement form = $(".form");
+        form.$("[data-test-id=name] input").setValue("Иванов Иван");
+        form.$(".button").click();
+        form.$(".input_invalid").$(".input__sub")
+                .shouldHave(Condition.exactText("Поле обязательно для заполнения"));
+    }
+
+    @Test
+    public void shouldValidateIncorrectTelField() {
+        open("http://localhost:9999");
+        SelenideElement form = $(".form");
+        form.$("[data-test-id=name] input").setValue("Иванов Иван");
+        form.$("[data-test-id=phone] input").setValue("+7123");
+        form.$(".button").click();
+        form.$(".input_invalid").$(".input__sub")
+                .shouldHave(Condition.exactText("Телефон указан неверно. Должно быть 11 цифр, например, +79012345678."));
+    }
+
+    @Test
+    public void shouldRequireAgreement() {
+        open("http://localhost:9999");
+        SelenideElement form = $(".form");
+        form.$("[data-test-id=name] input").setValue("Иванов Иван");
+        form.$("[data-test-id=phone] input").setValue("+71234567890");
+        form.$(".button").click();
+        form.$("[data-test-id=agreement]").shouldHave(Condition.cssClass("input_invalid"));
+    }
+
+}
